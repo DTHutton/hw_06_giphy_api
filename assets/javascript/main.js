5a930c92dd9b55e0a9dc6a414423b7dd5fd88990
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-07-14 16:19:14 -0400

    added button making functions

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,14 +1,35 @@
 $(document).ready(function () {
+    //variables and button array
+    let animals = ["pig", "cow", "lizard", "snake", "quokka"]
 
-    console.log('I hope this works.');
+    const apiKey = "gAw2fynIwAANSTYnFxo7kHEYbOEsfAov";
 
-
-    $('.jQuery').text('it works!')
-
-    const apiKey = 'gAw2fynIwAANSTYnFxo7kHEYbOEsfAov';
-    let buttonVal = 'cheeseburger'
+    let buttonVal = "cheeseburger"
 
     let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonVal + "&limit=10&api_key=" + apiKey;
+
+    //makes the buttons
+    makeButtons = () => {
+        $(".buttons").empty();
+        for (let i = 0; i < animals.length; i++) {
+            $(".buttons").append("<button>" + animals[i] + "</button>")
+        }
+    }
+
+    //makes buttons based on user input
+    $("#submitBtn").on("click", function (event) {
+        event.preventDefault();
+        let userInput = $("#textBox").val().trim();
+        animals.push(userInput);
+        makeButtons();
+    });
+
+
+
+    makeButtons();
+
+
+
 
     $.ajax({
         type: "GET",
