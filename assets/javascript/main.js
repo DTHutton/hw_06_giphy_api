1bc5b4d68adb6bcc25a10aab90a2b84bbd9fc97a
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-07-14 18:00:53 -0400

    changed makeButton function to add buttons via template string. template string adds data-name and new class successfully. stuck trying to get onclick based on new class added to work.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,41 +2,53 @@ $(document).ready(function () {
     //variables and button array
     let animals = ["pig", "cow", "lizard", "snake", "quokka"]
 
-    const apiKey = "gAw2fynIwAANSTYnFxo7kHEYbOEsfAov";
+    // const apiKey = "gAw2fynIwAANSTYnFxo7kHEYbOEsfAov";
 
-    let buttonVal = "cheeseburger"
+    // let buttonVal = "cheeseburger"
 
-    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonVal + "&limit=10&api_key=" + apiKey;
+    // let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonVal + "&limit=10&api_key=" + apiKey;
 
     //makes the buttons
     makeButtons = () => {
         $(".buttons").empty();
         for (let i = 0; i < animals.length; i++) {
-            $(".buttons").append("<button>" + animals[i] + "</button>")
+
+            const btnTemplate = `
+            <button data-name="${animals[i]}" class="gifBtn">
+                ${animals[i]}
+            </button>
+            `
+            $(".buttons").append(btnTemplate);
         }
     }
 
     //makes buttons based on user input
-    $("#submitBtn").on("click", function (event) { 
+    $("#submitBtn").on("click", function (event) {
         event.preventDefault();
         let userInput = $("#textBox").val().trim();
         animals.push(userInput);
         makeButtons();
     });
 
-
+    $(".gifBtn").on("click", function (event) {
+        event.preventDefault();
+        // $("button").attr("data-name")
+        console.log("test");
+    });
 
     makeButtons();
 
 
 
 
-    $.ajax({
-        type: "GET",
-        url: queryURL,
-        success: function (response) {
-            console.log(response);
-        }
-    });
+    // $.ajax({
+    //     type: "GET",
+    //     url: queryURL,
+    //     success: function (response) {
+    //         console.log(response);
+    //         let results = response.data;
+
+    //     }
+    // });
 
 });
